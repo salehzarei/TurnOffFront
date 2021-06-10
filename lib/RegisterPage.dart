596deb5f22456286209d85c30d1645bfd2825950
Saleh Zarei <saleh.zarei@gmail.com>
2021-06-10 19:42:30 +0430

    تغییرات جدید در اپلیکیشن 3/20

---
+++ lib/RegisterPage.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:turnoff/GetX/GetController.dart';
+import 'package:turnoff/VerificationCodePage.dart';
+import 'package:turnoff/homePage.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final x = Get.put(TurnOffController());
+    return GetX<TurnOffController>(
+        builder: (_) => Material(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "لطفا جهت دریافت پیامک اطلاع رسانی شماره تلفن همراه خود را وارد کنید",
+                      textAlign: TextAlign.center,
+                      textDirection: TextDirection.rtl,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                      child: TextField(
+                        controller: x.phoneRegContoller(),
+                        maxLength: 11,
+                        keyboardType: TextInputType.phone,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 30),
+                        decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide:
+                                  BorderSide(color: Colors.yellow, width: 2.0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 2.0),
+                            ),
+                            counterText: '',
+                            hintText: '09154127181',
+                            hintStyle: TextStyle(color: Colors.grey.shade300)),
+                      ),
+                    ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                          alignment: Alignment.center,
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              x.phoneRegContoller.value.text.length == 11
+                                  ? Colors.yellow.shade600
+                                  : Colors.yellow.shade100),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(13.0),
+                                      side: BorderSide(color: Colors.yellow)))),
+                      onPressed: () =>
+                          x.phoneRegContoller.value.text.length == 11
+                              ? Get.to(HomePage())
+                              : null,
+                      child: Text(
+                        "ثبت شماره تلفن",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                    ),
+                    Text(x.phoneRegContoller.value.text.toString())
+                  ],
+                ),
+              ),
+            ));
+  }
+}
